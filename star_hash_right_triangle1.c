a835532abbf70690475e1f3916ab05793eedad62
Chavan-Vaishnavi <vaishnavic11532gmail.com>
2026-07-19 23:23:12 +0530

    Another way of printing stars and hash on alternate lines in right angled triangle

---
+++ star_hash_right_triangle1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    int r, c;
+
+    for(r = 0; r < 4; r++)
+    {
+        for(c = 0; c <= r; c++)
+        {
+            if(r % 2 == 0)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf("@");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
